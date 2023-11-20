0cc41320e546e7d2d794705f3a02537e47a4c90b
lucas-faget <lucas.faget@laposte.net>
2023-11-20 22:30:03 +0100

    Profile card component

---
+++ frontend/src/components/ProfileCard.vue
@@ -0,0 +1,37 @@
+<script lang="ts">
+
+</script>
+
+<template>
+    <div class="profile-card">
+        <div class="background-image"></div>
+        <div class="profile-image"></div>
+    </div>
+</template>
+
+<style scoped>
+    .profile-card {
+        position: relative;
+    }
+
+    .background-image {
+        background: url("/banner_cv.png");
+        background-size: cover;
+        position: absolute;
+        width: 100%;
+        height: var(--background-image-height);
+        border-radius: 10px 10px 0 0;
+    }
+
+    .profile-image {
+        background: url("/avatar/6.svg");
+        background-size: cover;
+        position: absolute;
+        top: calc(var(--background-image-height) - (var(--profile-image-size) / 2));
+        left: calc(50% - (var(--profile-image-size) / 2));
+        width: var(--profile-image-size);
+        aspect-ratio: 1/1;
+        border: 3px solid var(--color-white);
+        border-radius: 100%;
+    }
+</style>
---
+++ frontend/src/views/HomePage.vue
@@ -0,0 +1,99 @@
+<script lang="ts" setup>
+    import ProfileCard from '../components/ProfileCard.vue'
+</script>
+
+<template>
+    <div class="homepage">
+        <div class="content">
+            <div class="left">
+                <ProfileCard />
+            </div>
+            <div class="middle">middle</div>
+            <div class="right">right</div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .homepage {
+        background: var(--color-light-gray);
+        padding-block: var(--section-gap);
+        min-height: 100vh;
+        display: flex;
+        justify-content: center;
+    }
+
+    .content {
+        display: grid;
+        grid-template-columns: repeat(3);
+        grid-gap: 20px;
+    }
+
+    .left, .middle, .right {
+        background: var(--color-white);
+        border-radius: 10px;
+    }
+
+    .left, .right {
+        width: var(--aside-width);
+    }
+
+    .middle {
+        width: var(--middle-section-width);
+    }
+
+    @media screen and (min-width: 1181px) {
+        .left {
+            grid-column: 1;
+            grid-row: 1;
+        }
+
+        .middle {
+            grid-column: 2;
+            grid-row: 1;
+        }
+
+        .right {
+            grid-column: 3;
+            grid-row: 1;
+        }
+    }
+
+    @media screen and (max-width: 1180px) {
+        .left {
+            grid-column: 1;
+            grid-row: 1;
+        }
+
+        .middle {
+            grid-column: 2 / 4;
+            grid-row: 1 / 4;
+        }
+
+        .right {
+            grid-column: 1;
+            grid-row: 2;
+        }
+    }
+
+    @media screen and (max-width: 860px) {
+        .left, .right {
+            width: var(--middle-section-width);
+        }
+
+        .left {
+            grid-column: 1 / 4;
+            grid-row: 1;
+        }
+
+        .middle {
+            grid-column: 1 / 4;
+            grid-row: 3;
+        }
+
+        .right {
+            grid-column: 1 / 4;
+            grid-row: 2;
+        }
+    }
+</style>
